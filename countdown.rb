00f838eb22ceec797acaeaacc12ca8fe9f7e7058
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-03-14 02:53:09 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  counter = 0
+  anoda_num= number
+  if number == 12
+    "HAPPY NEW YEAR!"
+  else
+    while counter < number
+      counter += 1
+      puts "#{anoda_num} SECOND(S)!"
+      anoda_num -= 1
+    end
+  end
+end
+
+def countdown_with_sleep(num)
+  num.times do
+    sleep(1)
+  end
 end
